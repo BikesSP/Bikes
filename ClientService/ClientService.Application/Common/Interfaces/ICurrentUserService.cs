fdfe4017dedd030d8e5d227267b7ac23c6605648
Tran Linh <tranlinhhqb@gmail.com>
2023-06-10 14:45:22 +0700

    Update Auth Sẻvice

--- ClientService/ClientService.Application/Common/Interfaces/ICurrentUserService.cs
+++ ClientService/ClientService.Application/Common/Interfaces/ICurrentUserService.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using ClientService.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +11,11 @@ namespace ClientService.Application.Common.Interfaces
 {
     public interface ICurrentUserService
     {
+        public string? CurrentPrincipal { get; }
+
+        public ClaimsPrincipal GetCurrentPrincipalFromToken(string token);
+
+        Task<Account> GetCurrentAccount(List<Expression<Func<Account, object>>> includes = null);
+
     }
 }
--- ClientService/ClientService.Infrastructure/Services/CurrentUserService.cs
+++ ClientService/ClientService.Infrastructure/Services/CurrentUserService.cs
@@ -1,14 +1,91 @@
-﻿using System.Linq.Expressions;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Linq.Expressions;
+using System.Security.Claims;
 using System.Security.Principal;
+using System.Text;
 using ClientService.Application.Common.Enums;
 using ClientService.Application.Common.Exceptions;
 using ClientService.Application.Common.Interfaces;
 using ClientService.Domain.Entities;
+using ClientService.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace ClientService.Infrastructure.Services;
 
 public class CurrentUserService : ICurrentUserService
 {
-    //TODO
+    private readonly IHttpContextAccessor _accessor;
+    private readonly IConfiguration _configuration;
+    private readonly IUnitOfWork _unitOfWork;
+    public CurrentUserService(IHttpContextAccessor accessor, IConfiguration configuration, IUnitOfWork unitOfWork)
+    {
+        _accessor = accessor;
+        _configuration = configuration;
+        _unitOfWork = unitOfWork;
+    }
+
+    // Get current login email
+    public string? CurrentPrincipal
+    {
+        get
+        {
+            var identity = _accessor?.HttpContext?.User.Identity as ClaimsIdentity;
+            if (identity == null || !identity.IsAuthenticated) return null;
+
+            var claims = identity.Claims;
+
+            var email = claims.FirstOrDefault(o => o.Type == ClaimTypes.NameIdentifier)?.Value ?? null;
+
+            return email;
+        }
+    }
+
+    public async Task<Account> GetCurrentAccount(List<Expression<Func<Account, object>>> includes = null)
+    {
+        var currentPrincipal = this.CurrentPrincipal;
+        if (currentPrincipal == null)
+        {
+            throw new ApiException(ResponseCode.Unauthorized);
+        }
+        var accountQuery =
+            await _unitOfWork.AccountRepository.GetAsync(
+                predicate: acc => acc.Email.ToLower().Equals(currentPrincipal.ToLower()),
+                includes: includes,
+                disableTracking: false);
+
+        var account = accountQuery.FirstOrDefault();
+
+        return account ?? throw new ApiException(ResponseCode.Unauthorized);
+    }
+
+
+    public ClaimsPrincipal GetCurrentPrincipalFromToken(string token)
+    {
+        var tokenValidationParams = new TokenValidationParameters()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = _configuration["Jwt:Issuer"],
+            ValidAudience = _configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"] ??
+                                                                               throw new ArgumentException(
+                                                                                   "Jwt:Key is required")))
+        };
+
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var principal = tokenHandler.ValidateToken(token, tokenValidationParams, out var securityToken);
+        var jwtSecurityToken = securityToken as JwtSecurityToken;
+        if (jwtSecurityToken == null || !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256,
+                StringComparison.InvariantCultureIgnoreCase))
+        {
+            throw new ApiException(ResponseCode.AuthErrorInvalidRefreshToken);
+        }
+
+        return principal;
+    }
 }
 
