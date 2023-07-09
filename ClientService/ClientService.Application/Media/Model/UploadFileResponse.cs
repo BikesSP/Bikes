6f754aa9cc59f30651a997c7a994796874460982
Nguyễn Thanh Hà <80661424+NTH260202@users.noreply.github.com>
2023-07-09 23:05:59 +0700

    Update Media Controller (#9)

---
+++ ClientService/ClientService.Application/Media/Model/UploadFileResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientService.Application.Media.Model
+{
+    public class UploadFileResponse
+    {
+        public string Path { get; set; }
+    }
+}
